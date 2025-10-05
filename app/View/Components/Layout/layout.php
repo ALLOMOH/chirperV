6dfec5bf7155c928bfcbb9550796cba9fa65d4c3
ALLOMOH <allomohangealex@gmail.com>
2025-10-05 09:12:53 +0000

    update the code

--- app/View/Components/Layout/layout.php
+++ app/View/Components/Layout/layout.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\View\Components\Layoute;
+namespace App\View\Components\Layout;
 
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class layoute extends Component
+class layout extends Component
 {
     /**
      * Create a new component instance.
